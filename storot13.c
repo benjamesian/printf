952169e161cb2d4a0fe48e64d49260d43b5fa95d
Ben Cook <808@holbertonschool.com>
2019-07-27 10:52:56 +0000

    function for rot13 encoding

---
+++ storot13.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encode a string in rot13
+ * @s: A string
+ *
+ * Return: Pointer to the beginning of s
+ */
+char *storot13(va_list valist)
+{
+	char w, c;
+	char *s, *dest;
+	int l, u, i;
+
+	s = va_arg(valist, char *);
+	dest = malloc(sizeof(*dest) * (_strlen(s) + 1));
+
+	c = *s;
+	i = 0;
+	while (c)
+	{
+		l = (c >= 'a' && c <= 'z');
+		u = (c >= 'A' && c <= 'Z');
+		w = (l * 'a') + (u * 'A');
+		if (w)
+			dest[i] = (((c - w) + 13) % 26) + w;
+		i++;
+		c = s[i];
+	}
+
+	return (dest);
+}
