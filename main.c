ff37a8cfc4db09829184455fa80f284552a087a7
Brent Muha <800@holbertonschool.com>
2019-07-27 20:30:13 +0000

    Simple main test file

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ int main(void)
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
+/*
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
@@ -40,5 +41,7 @@ int main(void)
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
+*/
+
 	return (0);
 }
