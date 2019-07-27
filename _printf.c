1c0ed43a3123d003bd5cfd922e49dddb435e7c41
Brent Muha <800@holbertonschool.com>
2019-07-27 19:04:23 +0000

    Update printf

--- _printf.c
+++ _printf.c
@@ -21,7 +21,7 @@ int get_substring_length(char *s)
 {
 	int len, is_escaped = 0;
 
-	for (len = 0; s[len] && !(s[len] == '%' && !is_escaped); len++)
+	for (len = 0; s[len] != '\0' && !(s[len] == '%' && !is_escaped); len++)
 		is_escaped = (s[len] == '\\' && !is_escaped);
 
 	return (len);
@@ -39,6 +39,25 @@ int get_specifier_length(char *s)
 	return (1);
 }
 
+/**
+ * get_printable_length - get printable length
+ * @s: string to print
+ *
+ * Return: printable length
+ */
+int get_printable_length(char *s)
+{
+	int i, j, is_escaped = 0;
+
+	for (i = 0, j = 0; s[i] != '\0'; i++, j++)
+	{
+		is_escaped = (s[len] == '\\' && !is_escaped);
+		if (is_escaped)
+			j--;
+	}
+
+	return (j);
+}
 
 /**
  * _printf - prints a formatted string
@@ -49,7 +68,7 @@ int get_specifier_length(char *s)
  */
 int _printf(const char *format, ...)
 {
-	int i, j = 0, n_strings = 0, in_string = 1, is_escaped = 0, format_len, total_length = 0, current_str_len, switch_index = 0;
+	int i, buffer_elements = 0, total_length = 0, current_len;
 	char next = '\0';
 	char **buffer;
 	va_list valist;
@@ -63,44 +82,31 @@ int _printf(const char *format, ...)
 		return (-2);
 
 	va_start(valist, format);
-	format_len = _strlen(format);
 
 /* get number of strings to store in buffer */
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0, buffer_elements = 0; format[i] != '\0'; i++)
 	{
-		is_escaped = (format[i] == '\\' && !is_escaped);
-		next = format[i + 1];
-
-		if (in_string)
-		{
-			if (!is_escaped && format[i] == '%')
-			{
-				in_string = 0;
-				n_strings++;
-				current_str_len = i - switch_index;
-				switch_index = i;
-				total_length += current_str_len;
-				buffer[j] = malloc();
-				if (!(j % 1024))
-				{/* move bffer */
-
-				}
-			}
-		}
+		if (format[i] == '%')
+			current_len = get_specifier_length(format + i);
 		else
 		{
-/*
- *			if (!is_valid_specifier_char(format[i]))
- *			{
- *				in_string = 1;
- *				n_strings++;
- *			}
- */
-			in_string = 1;
-			n_strings++;
-			switch_index = i;
+			current_len = get_substring_length(format + i);
+			
 		}
+		i += current_len;
+		buffer_elements++;
 	}
 
+	for (i = 0; i < buffer_elements; i++)
+	{
+		total_length += get_printable_length(buffer[i]);
+		print(buffer[i]);
+	}
+
+	print("\n");
+	total_length += 1;
+
 	va_end(valist);
+
+	return (total_length);
 }
