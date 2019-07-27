8c6b42789e6c33107af83abcb114a5b2e73fdf65
Ben Cook <808@holbertonschool.com>
2019-07-27 10:52:40 +0000

    function for reversing strings

---
+++ storev.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * storev - get a reversed copy of a string
+ * @valist: holds string to reverse
+ *
+ * Return: pointer to newly allocated reversed string
+ */
+char *storev(va_list valist)
+{
+	char *_s = va_arg(valist, char *);
+	char *s = _strdup(_s);
+
+	char temp;
+	int i, len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+
+	return (s);
+}
