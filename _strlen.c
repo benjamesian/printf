c3ad748349fbd7a8f6f60901e1179016eaf0d46b
Ben Cook <808@holbertonschool.com>
2019-07-27 06:16:21 +0000

    basic strlen

---
+++ _strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - Return the length of a string
+ * @s: The string to check
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
