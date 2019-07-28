f9cc069be465e76feac1796eba5574471b780384
Brent Muha <800@holbertonschool.com>
2019-07-28 20:57:33 +0000

    Add string to buffer

---
+++ string_to_buffer.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * string_to_buffer - Put a string in the buffer
+ * @s: string to put in the buffer
+ * @buffer: character buffer for printing
+ * @pos: position in the buffer
+ * @n_printed: number of printable characters
+ */
+void string_to_buffer(char *s, char *buffer, int *pos, int *n_printed)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		buffer_full(buffer, pos, n_printed);
+		buffer[*pos] = s[i];
+		pos++;
+	}
+}
