da9e868887c42d85735d9b62db33d49f26d87819
Ben Cook <808@holbertonschool.com>
2019-07-28 23:27:33 +0000

    print small buffers

--- _printf.c
+++ _printf.c
@@ -5,15 +5,17 @@
  * @format: format string containing strings and specifiers
  * @i: current position in format string
  * @width: width of element just read in format string
- * @valist: elements that will replace specifiers
- *
- * Return: On success the resulting element in a newly allocated string.
- * On error, NULL is returned.
+ * @valist: list with next argument
+ * @buffer: character buffer for printing
+ * @pos: position in the buffer
+ * @n_printed: number of printable characters
  */
-void get_next_elem(const char *format, int i, int *width, va_list valist, char *buffer, int *pos, int *n_printed)
+void get_next_elem(const char *format, int i, int *width, va_list valist,
+		   char *buffer, int *pos, int *n_printed)
 {
 	int j, current_len;
 	char *spec;
+	int (*type_to_buffer)(va_list, char *, int *, int *);
 
 	if (format[i] == '%')
 	{
@@ -22,8 +24,11 @@ void get_next_elem(const char *format, int i, int *width, va_list valist, char *
 		spec = malloc(sizeof(*spec) * current_len);
 		if (!spec)
 			exit(98);
+		_strncpy(spec, format + i + 1, current_len - 1);
+		spec[current_len - 1] = '\0';
 
-		get_type(spec)(valist, buffer, pos, n_printed);
+		type_to_buffer = get_type(spec);
+		type_to_buffer(valist, buffer, pos, n_printed);
 
 		free(spec);
 	}
@@ -50,14 +55,16 @@ void get_next_elem(const char *format, int i, int *width, va_list valist, char *
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, cur_len, pos = 0, n_printed = 0;
+	int i, cur_len, pos = 0, n_printed = 0, chars_to_print;
 	char buffer[BUFFER_SIZE];
 	va_list valist;
 
 	if (!format)
 		return (-1);
 
-	buffer[BUFFER_SIZE - 1] = '\0';
+	for (i = 0; i < BUFFER_SIZE; i++)
+		buffer[i] = '\0';
+	i = 0;
 
 	va_start(valist, format);
 	while (format[i] != '\0')
@@ -67,5 +74,12 @@ int _printf(const char *format, ...)
 	}
 	va_end(valist);
 
+/*
+ *	printf("ready to print\n");
+ *	printf("%s %d\n", buffer, get_printable_length(buffer));
+ */
+	chars_to_print = get_printable_length(buffer);
+	print(buffer, chars_to_print);
+	n_printed += chars_to_print;
 	return (n_printed);
 }
