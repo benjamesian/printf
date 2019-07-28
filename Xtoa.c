019140ff95bffd48e3428beb6a7dfc22db9cdc42
Brent Muha <800@holbertonschool.com>
2019-07-28 17:29:43 +0000

    Add Xtoa

---
+++ Xtoa.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * Xtoa - convert an unsigned int to hexadecimal string (uppercase)
+ * @valist: list with next argument
+ *
+ * Return: On success pointer to newly allocated string.
+ * On error, NULL is returned.
+ */
+char *Xtoa(va_list valist)
+{
+	int i, j, len;
+	char temp;
+	unsigned int b = va_arg(valist, unsigned int);
+	char *s = malloc(sizeof(char) * 33);
+
+	if (!s)
+		return (NULL);
+
+	i = 0;
+	while (b > 0)
+	{
+		s[i] = (b % 16);
+		if ((b % 16) < 10)
+			s[i] += '0';
+		else
+			s[i] += 'A' - 10;
+		b /= 16;
+		i++;
+	}
+
+	len = i;
+	for (j = i - 1; j >= len / 2; j--)
+	{
+		temp = s[j];
+		s[j] = s[len - j - 1];
+		s[len - j - 1] = temp;
+	}
+	s[len] = '\0';
+
+	return (s);
+}
