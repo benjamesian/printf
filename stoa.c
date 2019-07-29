9669bb37bb61c311c4b9e0df03724a76141b9f14
Ben Cook <808@holbertonschool.com>
2019-07-29 04:38:11 +0000

    null check argument

--- stoa.c
+++ stoa.c
@@ -13,6 +13,8 @@ int stoa(va_list valist, char *buffer, int *pos, int *n_printed)
 {
 	char *string = va_arg(valist, char *);
 
+	if (!string)
+		return (1);
 	string_to_buffer(string, buffer, pos, n_printed);
 
 	return (0);
