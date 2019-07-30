5ea7c35c9f1338d630ce149a44b269c4d8508b25
Brent Muha <800@holbertonschool.com>
2019-07-30 19:41:44 +0000

    Add spec to prototypes

--- Xtoa.c
+++ Xtoa.c
@@ -6,11 +6,12 @@
  * @buffer: character buffer for printing
  * @pos: position in the buffer
  * @n_printed: number of printable characters
+ * @spec: specifier flags
  *
  * Return: On success pointer to newly allocated string.
  * On error, NULL is returned.
  */
-int Xtoa(va_list valist, char *buffer, int *pos, int *n_printed)
+int Xtoa(va_list valist, char *buffer, int *pos, int *n_printed, char *spec)
 {
 	int i;
 	unsigned int b = va_arg(valist, unsigned int);
